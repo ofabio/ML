40d6892a6a5ae08fed02e8af2d6d31197ae79258
Fabio Pricoco <fabio.pricoco@intel.com>
2017-11-21 19:30:03 +0100

    clustering completed using k-means from sklearn

--- fabio.py
+++ fabio.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import matplotlib as ml
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 
 class Plotter:
@@ -28,8 +29,7 @@ class Plotter:
 
 
 def create_dataset(space_size, n_clusters, n_points_per_cluster):
-    statistic_space = 0.7 * space_size
-    distribution_centroids = np.random.rand(n_clusters, 2) * statistic_space
+    distribution_centroids = np.random.rand(n_clusters, 2) * (0.7 * space_size)
     points = None
     for c in distribution_centroids:
         cluster_points = np.random.randn(n_points_per_cluster, 2) + c
@@ -39,23 +39,42 @@ def create_dataset(space_size, n_clusters, n_points_per_cluster):
             points = np.concatenate((points, cluster_points), axis=0)
     return points
 
-def get_clusters(points):
-    init_centroid()
-    for point 
-    return clusters
+    # np.random.seed(200)
 
 
-plotter = Plotter(100, 100)
+
+# plotter = Plotter(100, 100)
+plt.ion()
+plt.axis([-100, 100, -100, 100])
 
 points = create_dataset(space_size=100, n_clusters=3, n_points_per_cluster=10)
+kmeans = KMeans(n_clusters=3, random_state=0).fit(points)
 
-drawn_points = plotter.drawn_point_vector(points, 'red')
-time.sleep(2)
-plotter.clear()
-drawn_points = plotter.drawn_point_vector(points, 'yellow')
-time.sleep(2)
+labels = kmeans.predict(points)
+centroids = kmeans.cluster_centers_
+print labels
+print centroids
 
 
+# drawn_points = plotter.drawn_point_vector(points, 'red')
+# time.sleep(2)
+# plotter.clear()
+# drawn_points = plotter.drawn_point_vector(points, 'yellow')
+# time.sleep(2)
+
+# fig = plt.figure(figsize=(5, 5))
+
+collist = ['r', 'g', 'b']
+colors = map(lambda x: collist[x], labels)
+
+plt.scatter(points[:, 0], points[:, 1], color=colors, alpha=0.5, edgecolor='k')
+for idx, centroid in enumerate(centroids):
+    plt.scatter(*centroid, color=collist[idx])
+
+# plt.pause(0.0001)
+# time.sleep(10)
+plt.show(block=True)
+# time.sleep(10)
 
 
 
